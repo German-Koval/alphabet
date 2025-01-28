7db93d691259bc3ecde97e03e488a427ffc595a5
German-Koval <germankovalenkophp@gmail.com>
2025-01-28 22:19:36 +0400

    CI

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const BASE_PATH = process.env.BASE_PATH
+
 export default defineConfig({
+  base: BASE_PATH ? `${BASE_PATH}/` : '/',
   plugins: [react()],
-  base: "/alphabet/"
 })
